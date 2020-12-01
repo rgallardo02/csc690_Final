8a8e69c86a7694fb2201a0a3e45b3b20e2ae4f8f
rodrigogallardo <rigogallardo10@gmail.com>
2020-12-01 15:33:49 -0800

    Launch Screen and Main Screeen Implemented

--- csc690_Final/ViewController.swift
+++ csc690_Final/ViewController.swift
@@ -2,18 +2,60 @@
 //  ViewController.swift
 //  csc690_Final
 //
-//  Created by Rodrigo Gallardo on 11/17/20.
+//  Created by Rodrigo Gallardo and German Perez on 11/17/20.
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    fileprivate let locationManager: CLLocationManager = {
+      let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        return manager
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         setUpMapView()
     }
+    
+    func setUpMapView(){
+        mapView.showsUserLocation = true
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        currentLocation()
+    }
+    
+    func currentLocation(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        if #available(iOS 11.0, *){
+            locationManager.showsBackgroundLocationIndicator = true
+        }else {
+            
+        }
+        locationManager.startUpdatingLocation()
+    }
+}
 
-
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        let location = locations.last! as CLLocation
+        let currentLocation = location.coordinate
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 800, longitudinalMeters: 800)
+        mapView.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
+        print(error.localizedDescription)
+    }
+    
+    
+    
 }
 
