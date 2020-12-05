42bf3cbb580a1b5307e2d6f150f1be9f4ac3eb9f
German Perez <germanperez@Germans-MacBook-Pro.local>
2020-12-04 21:54:30 -0800

    implement pin

--- csc690_Final/ViewController.swift
+++ csc690_Final/ViewController.swift
@@ -21,6 +21,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
          setUpMapView()
+        
+        mapView.delegate = self
+        let longTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
+        mapView.addGestureRecognizer(longTapGesture)
+    }
+    
+    @objc func longTap(sender: UIGestureRecognizer){
+        print("long tap")
+        if sender.state == .began {
+            let locationInView = sender.location(in: mapView)
+            let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+            addAnnotation(location: locationOnMap)
+        }
+    }
+
+    func addAnnotation(location: CLLocationCoordinate2D){
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            annotation.title = "Some Title"
+            annotation.subtitle = "Some Subtitle"
+            self.mapView.addAnnotation(annotation)
     }
     
     func setUpMapView(){
@@ -58,4 +79,35 @@ extension ViewController: CLLocationManagerDelegate {
     
     
 }
+extension ViewController: MKMapViewDelegate{
 
+func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard annotation is MKPointAnnotation else { print("no mkpointannotaions"); return nil }
+
+    let reuseId = "pin"
+    var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+    if pinView == nil {
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView!.canShowCallout = true
+        pinView!.rightCalloutAccessoryView = UIButton(type: .infoDark)
+        pinView!.pinTintColor = UIColor.black
+    }
+    else {
+        pinView!.annotation = annotation
+    }
+    return pinView
+}
+
+func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+    print("tapped on pin ")
+}
+
+func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+    if control == view.rightCalloutAccessoryView {
+        if let doSomething = view.annotation?.title! {
+           print("do something")
+        }
+    }
+  }
+}
